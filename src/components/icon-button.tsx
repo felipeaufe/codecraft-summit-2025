59ba61aa3c4571c85306ce3ce46a3c714ddd9607
Felipe Augusto <felipe.aufe@gmail.com>
2025-02-18 18:03:31 -0300

    feat: add invite page and form validation

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -1,12 +1,16 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 interface IconButtonProps extends ComponentProps<'button'> {}
 
-const IconButton = (props: IconButtonProps) => {
+const IconButton = ({ className, ...rest }: IconButtonProps) => {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue font-semibold rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
-      {...props}
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue font-semibold rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300',
+        className
+      )}
+      {...rest}
     />
   )
 }
