b87df89f116bcc35d5d206c55f265eaead92bdc6
Felipe Augusto <felipe.aufe@gmail.com>
2025-02-19 13:54:41 -0300

    feat: add api integration

--- src/app/invite/[subscriberId]/stats.tsx
+++ src/app/invite/[subscriberId]/stats.tsx
@@ -1,11 +1,25 @@
+import {
+  getRanking,
+  getSubscriberInviteClicks,
+  getSubscriberInviteCount,
+  getSubscriberRankingPosition,
+} from '@/http/api'
 import { BadgeCheck, Medal, MousePointerClick } from 'lucide-react'
 
-export const Stats = () => {
+interface StatsProps {
+  readonly subscriberId: string
+}
+export async function Stats({ subscriberId }: StatsProps) {
+  const { count: accessCount } = await getSubscriberInviteClicks(subscriberId)
+  const { count: inviteCount } = await getSubscriberInviteCount(subscriberId)
+  const { position: rankingPosition } =
+    await getSubscriberRankingPosition(subscriberId)
+
   return (
     <div className="grid gap-3 md:grid-cols-3">
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          1042
+          {accessCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
           Acessos ao link
@@ -14,19 +28,19 @@ export const Stats = () => {
       </div>
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          1042
+          {inviteCount}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
-          Acessos ao link
+          Inscrições feitas
         </span>
         <BadgeCheck className="size-5 text-purple absolute left-3 top-3" />
       </div>
       <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl">
         <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-          1042
+          {rankingPosition ? `${rankingPosition}º` : '-'}
         </span>
         <span className="text-sm text-gray-300 leading-none text-center">
-          Acessos ao link
+          Posição no ranking
         </span>
         <Medal className="size-5 text-purple absolute left-3 top-3" />
       </div>
